77a0d15347ba92ff4cd00c1c07841a2da8e194de
Bertrand Le Roy <beleroy@microsoft.com>
2016-05-13 13:30:25 -0700

    Add golden project

---
+++ Golden/src/Library/Thing.cs
@@ -0,0 +1,9 @@
+﻿namespace Library
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Thing
+    {
+        public int Get(int number) => Newtonsoft.Json.JsonConvert.DeserializeObject<int>($"{number}");
+    }
+}
---
+++ Golden/test/TestApp/LibraryTests.cs
@@ -0,0 +1,13 @@
+﻿using Library;
+using Xunit;
+
+namespace TestApp
+{
+    public class LibraryTests
+    {
+        [Fact]
+        public void TestThing() {
+            Assert.Equal(42, new Thing().Get(42));
+        }
+    }
+}

0ea8f9adc161d2057e09bd9b6d7d01338523c448
Bertrand Le Roy <beleroy@microsoft.com>
2016-05-13 17:20:08 -0700

    Start golden CLI script

---
+++ GoldenCLI/src/Library/Thing.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Library
+{
+    public class Thing
+    {
+        public int Get(int number) => Newtonsoft.Json.JsonConvert.DeserializeObject<int>($"{number}");
+    }
+}

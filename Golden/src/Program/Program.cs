fbd84c942c36ee0b9d8193af5432d6958f2fb5b0
Brett Morrison <b@brettmorrison.com>
2016-05-14 12:21:49 -0700

    Added 'Program' project to solution.

---
+++ Golden/src/Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Library;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine($"The answer to everything is: {new Thing().Get(42)}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+        }
+    }
+}
---
+++ Golden/src/Program/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Program")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dacf1569-8a7f-4417-8876-e8f3511942e3")]
